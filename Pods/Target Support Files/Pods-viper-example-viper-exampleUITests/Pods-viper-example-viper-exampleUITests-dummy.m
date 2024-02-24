8b77308f9974ea38779b43f4aa3a95bc21e17be3
Revan SADIGLI <revan.sadiqli99@gmail.com>
2024-02-25 02:57:50 +0300

    vcs: remove Pods/ directory from Git tracking

--- Pods/Target Support Files/Pods-viper-example-viper-exampleUITests/Pods-viper-example-viper-exampleUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_viper_example_viper_exampleUITests : NSObject
-@end
-@implementation PodsDummy_Pods_viper_example_viper_exampleUITests
-@end
--- Pods/Target Support Files/Pods-viper-example/Pods-viper-example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_viper_example : NSObject
-@end
-@implementation PodsDummy_Pods_viper_example
-@end
